d0711da285c1eb41c23968aa7de3bf98dca6a6bb
Laxman Desai <lolopaw24@gmail.com>
2022-06-12 05:19:47 +0530

    👌 IMPROVE: Add gh-pages and update deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,10 +11,9 @@ const config = {
 
   kit: {
     adapter: adapter(),
-
-    // Override http methods in the Todo forms
-    methodOverride: {
-      allowed: ['PATCH', 'DELETE'],
+    trailingSlash: 'ignore',
+    prerender: {
+      default: true,
     },
   },
 };
